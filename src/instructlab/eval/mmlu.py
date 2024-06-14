3558524b8a589cdd92a359ab505f1e4b3bedf791
Nathan Weinberg <nweinber@redhat.com>
2024-06-14 11:28:21 -0400

    Add additional child classes for PR-Bench and PR-MMLU
    Signed-off-by: Nathan Weinberg <nweinber@redhat.com>

--- src/instructlab/eval/mmlu.py
+++ src/instructlab/eval/mmlu.py
@@ -4,7 +4,7 @@
 from .evaluator import Evaluator
 
 
-class MMLUEvaluator(Evaluator):
+class MMLU_Evaluator(Evaluator):
     """
     Child class of an Evaluator for Massive Multitask Language Understanding (MMLU)
     """
@@ -12,5 +12,20 @@ class MMLUEvaluator(Evaluator):
     def __init__(self, model, tasks: list[str], fewshots: int, batchsize: int) -> None:
         super().__init__(model)
         self.tasks = tasks
+        self.fewshots = fewshots
+        self.batchsize = batchsize
+
+
+class PR_MMLU_Evaluator(Evaluator):
+    """
+    Child class of an Evaluator for PR Massive Multitask Language Understanding (PR MMLU)
+    """
+
+    def __init__(
+        self, model, task: str, sdg_path: str, fewshots: int, batchsize: int
+    ) -> None:
+        super().__init__(model)
+        self.task = task
+        self.sdg_path = sdg_path
         self.fewshots = fewshots
         self.batchsize = batchsize
--- src/instructlab/eval/mtbench.py
+++ src/instructlab/eval/mtbench.py
@@ -4,7 +4,7 @@
 from .evaluator import Evaluator
 
 
-class MTBenchEvaluator(Evaluator):
+class MT_Bench_Evaluator(Evaluator):
     """
     Child class of an Evaluator for Multi-turn Benchmark (MT-Bench)
     """
@@ -12,3 +12,14 @@ class MTBenchEvaluator(Evaluator):
     def __init__(self, model, server: str) -> None:
         super().__init__(model)
         self.server = server
+
+
+class PR_Bench_Evaluator(Evaluator):
+    """
+    Child class of an Evaluator for PR-Bench Benchmark (PR-Bench)
+    """
+
+    def __init__(self, model, server: str, questions: str) -> None:
+        super().__init__(model)
+        self.server = server
+        self.questions = questions
