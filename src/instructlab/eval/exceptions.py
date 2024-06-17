b422048b912cc065e2c800f1cfe1d48a5c38b29b
Nathan Weinberg <nweinber@redhat.com>
2024-06-17 10:19:50 -0400

    Update attributes of ModelNotFoundError class
    Signed-off-by: Nathan Weinberg <nweinber@redhat.com>

--- src/instructlab/eval/exceptions.py
+++ src/instructlab/eval/exceptions.py
@@ -12,10 +12,13 @@ class ModelNotFoundError(EvalError):
     Exception raised when model is not able to be found
 
     Attributes
-        model   model that is being operated on
+        message     error message to be printed on raise
+        model       model that is being operated on
+        path        filepath of model location
     """
 
-    def __init__(self, model) -> None:
+    def __init__(self, path) -> None:
         super().__init__()
-        self.model = model
-        self.message = f"Model {self.model} could not be found"
+        self.path = path
+        self.model = path.rsplit("/")[-1]
+        self.message = f"Model {self.model} could not be found at {self.path}"
