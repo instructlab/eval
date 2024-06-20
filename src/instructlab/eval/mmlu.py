8e024b80522eae080bbdbc0c5922540c232a146d
Ali Maredia <amaredia@redhat.com>
2024-06-20 11:07:41 +0000

    working MMLU_Evaluator.run()
    Replicates functionality in backend evaluation
    code for mmlu.
    
    Model that is tested is served my lm-eval code
    internally.
    
    Signed-off-by: Ali Maredia <amaredia@redhat.com>

--- src/instructlab/eval/mmlu.py
+++ src/instructlab/eval/mmlu.py
@@ -1,28 +1,30 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # Local
-from .evaluator import Evaluator
+from instructlab.eval.evaluator import Evaluator
 
 # Third Party 
 from lm_eval.evaluator import simple_evaluate
+import os
 
 class MMLU_Evaluator(Evaluator):
     """
     Child class of an Evaluator for Massive Multitask Language Understanding (MMLU)
 
     Attributes:
+        model_path   absolute path to or name of a huggingface model
         tasks        list of tasks for MMLU to test the model with
+        model_dtype  dtype of model when served
         few_shots    number of examples
         batch_size   number of GPUs
     """
 
     def __init__(
-        self, model, model_args, model_path, tasks: list[str], few_shots: int = 2, batch_size: int = 5
+        self, model_path, tasks: list[str], model_dtype = 'bfloat16', few_shots: int = 2, batch_size: int = 5
     ) -> None:
         super().__init__(model_path)
-        self.model = model
-        self.model_args = model_args
         self.tasks = tasks
+        self.model_dtype = model_dtype
         self.few_shots = few_shots
         self.batch_size = batch_size
 
@@ -34,35 +36,45 @@ class MMLU_Evaluator(Evaluator):
             overall_score       MMLU score for the overall model evaluation
             individual_scores   Individual MMLU score for each task
         """
-        individual_scores: dict[str, float] = {}
-        overall_score: float = 0.0
-        results = lm_eval.simple_evaluate(
-            model=self.model,
-            model_args=self.model_args,
+        #TODO: make this a parameter for class?
+        os.environ['TOKENIZERS_PARALLELISM'] = 'true'
+
+        individual_scores: dict = {}
+        agg_score: float = 0.0
+        model_args = "pretrained=" + self.model_path + ",dtype=" + self.model_dtype
+
+        mmlu_output = simple_evaluate(
+            model="hf",
+            model_args=model_args,
             tasks=self.tasks,
             num_fewshot=self.few_shots,
-            batch_size=self.batch_size,
-            log_samples=True,
+            batch_size=self.batch_size
         )
-        #TODO: see what the output of results looks like 
-        #print(results)
-        #calculate_overall_score(results)
+
+        results = mmlu_output["results"]
+
+        for task in self.tasks:
+            mmlu_res = results[task]
+            agg_score += float(mmlu_res['acc,none'])
+            individual_scores[task] = {}
+            individual_scores[task]['score'] = float(mmlu_res['acc,none'])
+            individual_scores[task]['stderr'] = float(mmlu_res['acc_stderr,none'])
+
+        overall_score = float(agg_score/len(self.tasks))
         return overall_score, individual_scores
     
-    def calculate_overall_score(scores):
-        pass # Placeholder for calculating overall score:
-             # overall score = (num model answered correctly / num questions)
 
 ############# Testing Code Follows ##############
 def main():
-    # TODO: change this- cli uses HuggingFace to access the model 
-    model = "hf"
-    model_args = "pretrained=$MODEL_PATH,dtype=bfloat16"
     # Path to the granite model in the aliryan vm on AWS
     model_path = "/home/ec2-user/instructlab/models/instructlab/granite-7b-lab"
     #TODO: all 57 tasks need to be parameterized possibly by CLI
-    tasks = "mmlu_abstract_algebra"
-    mmlu = MMLU_Evaluator(model, model_args, model_path, tasks, 2, 5)
+    tasks = ["mmlu_abstract_algebra","mmlu_anatomy","mmlu_astronomy"]
+    dtype = "float16"
+    mmlu = MMLU_Evaluator(model_path, tasks, dtype, 2, 5)
+    overall_score, individual_scores = mmlu.run()
+    print(overall_score)
+    print(individual_scores)
 
 if __name__ == "__main__":
     main()
