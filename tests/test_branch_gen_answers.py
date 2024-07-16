4f4be693eb75462f9aac7a4bbf817932b44da467
Nathan Weinberg <nweinber@redhat.com>
2024-07-16 10:17:54 -0500

    [WIP] Add new GPU runner for E2E tests
    Use previous GPU runner for unit tests
    
    Signed-off-by: Nathan Weinberg <nweinber@redhat.com>

--- tests/test_branch_gen_answers.py
+++ tests/test_branch_gen_answers.py
@@ -1,10 +1,12 @@
 # First Party
 from instructlab.eval.mt_bench import MTBenchBranchEvaluator
 
-mt_bench_branch = MTBenchBranchEvaluator(
-    "instructlab/granite-7b-lab",
-    "instructlab/granite-7b-lab",
-    "../taxonomy",
-    "main",
-)
-mt_bench_branch.gen_answers("http://localhost:8000/v1")
+
+def test_branch_gen_answers():
+    mt_bench_branch = MTBenchBranchEvaluator(
+        "instructlab/granite-7b-lab",
+        "instructlab/granite-7b-lab",
+        "taxonomy",
+        "main",
+    )
+    mt_bench_branch.gen_answers("http://localhost:8000/v1")
